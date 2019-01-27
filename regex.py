a4575ae327969a75133f64e1677ea65498d86b53
Marc Sandifer <m.j.sandifer@googlemail.com>
2019-01-27 18:49:44 +0100

    v0.5 - UI MVP functional

--- regex.py
+++ regex.py
@@ -78,33 +78,6 @@ class ReplaceWindow(QDialog, Ui_replaceEntry):
         self.close()
         super(ReplaceWindow, self).reject()   # call the reject method, as we're overwriting it
 
-# def filename_prompt():
-#        file_name = ""
-#        while "." not in file_name or len(file_name) < 3:                               # to-do: replace with regEx
-#            file_name = input("Please enter the name of your file.\n"
-#                              "Files must have an extension and the name must be at least 3 characters long: ")
-#        return file_name
-
-
-# def regex_prompt():
-#    regex = []
-#    regex.append(input("Please enter your regular expression: "))
-#    yes_no = input("Would you like to add another regular expression to apply?")
-#    while yes_no.lower() not in {"n", "y"}:
-#        yes_no = input("Please answer with 'n' or 'y': ")
-#    while yes_no.lower() == "y":
-#        regex.append(input("Please enter your regular expression: "))
-#        yes_no = input("Would you like to add another regular expression to apply?")
-#    print(f"You've entered {len(regex)} regular expression(s).")
-#    return regex
-
-
-# def replacement_prompt(regex):
-#    replacement = []
-#    for expression in regex:
-#        replacement.append(input(f"Please enter what you'd like to replace {expression} with: "))
-#    return replacement
-
 
 def regex_this(file_name, regex, replacement):
     split_filename, split_extension = file_name.split(".")
@@ -132,9 +105,6 @@ def main():
     w = AppWindow()
     w.show()
     sys.exit(app.exec_())
-    # regex = regex_prompt()
-    # replacement = replacement_prompt(regex)
-    # regex_this(filename, regex, replacement)
 
 
 if __name__ == '__main__':
